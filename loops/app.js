58206c60c092e789b4093f460cb1785de0a3fe2f
talhahzahid <talhazahid415@gmail.com>
2024-03-14 12:35:26 +0500

    index file

--- loops/app.js
+++ loops/app.js
@@ -1,5 +1,39 @@
 
 
+// create a table with the help of loop
+
+// const userInput = +prompt("Enter A Table");
+// const times = +prompt("How Many Times Do you Want Table");
+
+// for ( let i = 1; i <= times ; i++){
+//     console.log(`${userInput} * ${i} = ${userInput*i}`)
+// }
+
+// table loop end 
 
 
 
+// let userInput = document.querySelector("#input1");
+// let times = document.querySelector("#input2");
+// let  get= document.querySelector("#get");
+
+// function generate(){
+//     for ( let i = 1; i <= times.value ; i++){
+//     console.log(` ${userInput.value} * ${i} = ${userInput.value*i}`);
+//     }
+// }   
+
+
+
+
+
+
+
+// let fruits = ['apple' , 'mango' , 'grapes', 'pineapple']
+// console.log(fruits);
+// const ul = document.querySelector('ul');
+
+// for ( let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i])
+//     ul.innerHTML += `<li>${fruits[i]}</li>`
+// }
