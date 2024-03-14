1e8fb4d7f1f4344cfc64340c0f367790925d1007
talhahzahid <talhazahid415@gmail.com>
2024-03-14 15:58:32 +0500

    index file

--- loops/app.js
+++ loops/app.js
@@ -37,3 +37,58 @@
 //     console.log(fruits[i])
 //     ul.innerHTML += `<li>${fruits[i]}</li>`
 // }
+
+
+
+
+// Question no 4
+// You have an array
+//  A = [“Nokia”, “Samsung”, “Apple”, “Sony”, “Huawei”]
+// Write each element on new line with the help of for loop.
+
+// let phone = ['Nokia', 'Samsung' , 'Apple' , "sony" , "huawei"]
+
+// for ( let i = 0; i < phone.length; i++){
+//     console.log(phone[i]);
+// }
+
+
+// question no Five 
+// 5. Write a program to print items of the following array using for 
+// loop:
+// fruits = [“apple”, “banana”, “mango”, “orange”, “strawberry”]
+
+// ANSWER
+
+// let fruits = ['apple','banana','orange','mango']
+
+// for ( let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i])
+// }
+// console.log("element at index at", fruits.indexOf('apple'),"is apple")
+// console.log("element at index at", fruits.indexOf('banana'), "is banana")
+// console.log("element at index at", fruits.indexOf('orange'), "is orange")
+// console.log("element at index at", fruits.indexOf('mango'), "is mango   ")
+
+
+
+
+
+
+
+
+// for (let num = 0; num <= 20; num++) {
+//     if (num % 2 === 0) {
+//         console.log(num + " is even");
+//     } else {
+//         console.log(num + " is odd");
+//     }
+// }
+
+
+ 
+
+
+
+
+
